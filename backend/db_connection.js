3d1de8c33e68ba97957c20fd82a79a8d344b4675
sesquivel0715 <sesquivel0715@gmail.com>
2026-05-06 23:13:17 -0700

    update database connection file

--- backend/db_connection.js
+++ backend/db_connection.js
@@ -1,12 +1,15 @@
 //database connection 
 
-//uses better-sqlite3to connect to sqlite
 const Database = require('better-sqlite3');
 
+const path = require('path');
+const dbPath = path.join(__dirname, '../database/ecommerce.db'); 
 let database;
 
 try { 
-    database = new Database('../database/ecommerce.db');
+    database = new Database(dbPath);
+    //needed for sqlite
+    database.pragma('foreign_keys = ON');
     console.log('connected successfully');
 } catch (error){
     console.error('failed to connect ', error.message);
